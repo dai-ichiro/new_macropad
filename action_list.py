903fd729ded2775d5b3ae0c03c0773f4a6e26ed2
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2022-06-07 21:53:32 +0900

    Update action_list.py

--- action_list.py
+++ action_list.py
@@ -1,66 +1,63 @@
-import pyautogui as pg
-import pyperclip as clip
+import keyboard as kb
 
 def action_do(x: int) -> None:
     match x:
         #PowerPoint settings
         case 0:
-            pg.press(['alt', 'n', 'x', 'h'])
+            kb.send('alt, n, x, h')
         case 1:
-            pg.press(['alt', 'n', 'p', 'd'])
+            kb.send('alt, n, p, d')
         case 2:
-            pg.press(['alt', 'n', 's', 'h']) 
+            kb.send('alt, n, s, h')
         case 3:
-            pg.press(['alt', 'h', 'g', 'a', 't'])
+            kb.send('alt, h, g, a, t')
         case 4:
-            pg.press(['alt', 'h', 'g', 'a', 'l'])
+            kb.send('alt, h, g, a, l')
         case 5:
-            pg.press(['alt', 'h', 'g', 'a', 'r'])
+            kb.send('alt, h, g, a, r')
         case 6:
-            pg.press(['alt', 'h', 'g', 'a', 'b'])
+            kb.send('alt, h, g, a, b')
         case 7:
-            pg.press(['alt', 'h', 'g', 'a', 'c'])
+            kb.send('alt, h, g, a, c')
         case 8:
-            pg.press(['alt', 'h', 'g', 'a', 'm'])
+            kb.send('alt, h, g, a, m')
         case 9:
-            pg.press(['alt', 'h', 'g', 'a', 'h'])
+            kb.send('alt, h, g, a, h')
         case 10:
-            pg.press(['alt', 'h', 'g', 'a', 'v'])
+            kb.send('alt, h, g, a, v')
 
         #YouTube Settings
         case 20:
-            pg.press('j')
+            kb.send('j')
         case 21:
-            pg.press('l')
+            kb.send('l')
         case 22:
-            pg.press('k')
+            kb.send('k')
         case 23:
-            pg.press('f')
+            kb.send('f')
 
         #Keyboard settings
         case 40:
-            pg.write('<h2></h2>')
-            pg.press(['left', 'left', 'left', 'left', 'left'])
+            kb.write('<h2></h2>')
+            kb.send('left, left, left, left, left')
         case 41:
-            pg.write('<h3></h3>')
-            pg.press(['left', 'left', 'left', 'left', 'left'])
+            kb.write('<h3></h3>')
+            kb.send('left, left, left, left, left')
         case 42:
-            pg.write('<h4></h4>')
-            pg.press(['left', 'left', 'left', 'left', 'left'])
+            kb.write('<h4></h4>')
+            kb.send('left, left, left, left, left')
         case 43:
-            pg.write('</br>')
+            kb.write('</br>')
         case 44:
-            pg.write('</br></br>')
+            kb.write('</br></br>')
         case 45:
-            pg.write('<a href=""></a>')
-            pg.press(['left', 'left', 'left', 'left', 'left', 'left'])
+            kb.write('<a href=""></a>')
+            kb.send('left, left, left, left, left, left')
         case 46:
-            pg.write('python -m pip install --upgrade pip')
+            kb.write('python -m pip install --upgrade pip')
         case 47:
-            pg.write('--cache-dir python_cache')
+            kb.write('--cache-dir python_cache')
         case 48:
-            clip.copy('pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu113')
-            pg.hotkey('ctrl', 'v')
+            kb.write('pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu113')
         case 49:
-            pg.write('abc_abc_abc')
-            
+            kb.write('ghp_m8h5rdj5dd98yMV9oVX4Iwge06C8EV9iy')
